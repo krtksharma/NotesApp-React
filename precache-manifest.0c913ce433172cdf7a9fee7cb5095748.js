6faf07be2d7d85420cf60f8147a17fa23f836ac2
krtksharma <krtksharma7@gmail.com>
2021-11-20 19:53:57 +0530

    Updates

---
+++ precache-manifest.0c913ce433172cdf7a9fee7cb5095748.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3e61e42d0e438dfa39bf45e7c88df119",
+    "url": "/NotesApp-React/index.html"
+  },
+  {
+    "revision": "2d21a79c995b45f19680",
+    "url": "/NotesApp-React/static/js/2.3a22d22b.chunk.js"
+  },
+  {
+    "revision": "66327269c9d317ea7db9",
+    "url": "/NotesApp-React/static/js/main.34c54a17.chunk.js"
+  },
+  {
+    "revision": "27acb160a990180be0c0",
+    "url": "/NotesApp-React/static/js/runtime-main.76caa121.js"
+  }
+]);
